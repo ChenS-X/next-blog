79ed37d19cbf1e698dd28180791a1d4e5ddeba4c
ChenSX <chensxyouxiang@163.com>
2026-03-23 22:12:33 +0800

    'feature:修改yml'

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/next-blog',
   images: {
     unoptimized: true, // Required for static export
   },
