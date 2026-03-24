023819cde4c073b55c863fe7b85b2e6e0858996f
ChenSX <chensxyouxiang@163.com>
2026-03-24 13:50:52 +0800

    chore: stop tracking .next directory

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+E+WBCLBEDT7QSgN/WyU+l1r2blMFcdMWmMCphbdnB4=\"\n}"
--- .next/dev/static/chunks/src_app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__00mef58._.css","static/chunks/node_modules_next_0l-kvsg._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0ezz.e5._.js","static/chunks/node_modules_motion-dom_dist_es_0n_wqqr._.js","static/chunks/node_modules_framer-motion_dist_es_129u8xh._.js","static/chunks/node_modules_08er4wg._.js","static/chunks/src_0t4sv3a._.js"],
-    source: "dynamic"
-});
